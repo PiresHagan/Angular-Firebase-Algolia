d34b49b8760b65177c62bea33014f9dcd64ab72c
PiresHagan <apollot7dev@gmail.com>
2020-04-27 19:26:02 +0530

    commit changes for home page integration

--- src/app/shared/template/header/header.component.ts
+++ src/app/shared/template/header/header.component.ts
@@ -3,7 +3,8 @@ import { ThemeConstantService } from '../../services/theme-constant.service';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 
 export class HeaderComponent{
