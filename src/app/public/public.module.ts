ea77d426301d0caac505bc49fb04aa937c6fac31
PiresHagan <apollot7dev@gmail.com>
2020-06-06 00:55:14 -0400

    leadfox integration

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
@@ -37,6 +37,7 @@ import { InfluencerComponent } from './influencer/influencer.component';
     TranslateModule.forChild({ useDefaultLang: true, isolate: false, loader: { provide: TranslateLoader, useFactory: (createTranslateLoader), deps: [HttpClient] } }),
     NzCollapseModule,
     NgAisModule.forRoot(),
-  ]
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class PublicModule { }
