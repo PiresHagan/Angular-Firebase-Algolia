62bce38bbc6e917fd949cdd3f12ac1d9580d4af2
PiresHagan <apollot7dev@gmail.com>
2020-07-02 17:48:24 +0530

    Added ssr_dev function for dev environment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
 const universal = require(`${process.cwd()}/dist/enlink/server/main`).app();
 
+export const ssr_dev = functions.https.onRequest(universal);
 export const ssr = functions.https.onRequest(universal);
