428b43d03d56fd3e7f1cb77736fb5b77c363fac2
PiresHagan <apollot7dev@gmail.com>
2020-08-28 02:50:15 -0400

    changing trending on home

--- src/app/shared/services/article.service.ts
+++ src/app/shared/services/article.service.ts
@@ -268,7 +268,7 @@ export class ArticleService {
       .where('status', "==", ACTIVE)
       .orderBy('published_at', 'desc')
       .orderBy('view_count', 'desc')
-      .limit(200)
+      .limit(50)
     ).snapshotChanges().pipe(
       map(actions => {
         return actions.map(a => {
