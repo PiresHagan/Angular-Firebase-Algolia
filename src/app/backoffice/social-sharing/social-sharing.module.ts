e791cf27ed5d0b403f452b87407fca132c673379
PiresHagan <apollot7dev@gmail.com>
2020-09-07 19:32:22 +0530

    added list component in social sharing

--- src/app/backoffice/social-sharing/social-sharing.module.ts
+++ src/app/backoffice/social-sharing/social-sharing.module.ts
@@ -10,8 +10,6 @@ import { SharedModule, createTranslateLoader } from 'src/app/shared/shared.modul
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 
-
-
 @NgModule({
   declarations: [CreatePostComponent, PostListComponent],
   imports: [
