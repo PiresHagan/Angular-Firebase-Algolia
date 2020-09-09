0ae72ff7f197c413c7838d806e759653c9691c1f
PiresHagan <apollot7dev@gmail.com>
2020-09-09 03:23:56 -0400

    fixing cloudinary https

--- src/app/backoffice/articles/article-single/article-single.component.ts
+++ src/app/backoffice/articles/article-single/article-single.component.ts
@@ -120,7 +120,7 @@ export class ArticleSingleComponent implements OnInit {
     let latestURL = url
     if (url) {
       latestURL = latestURL.replace('https://mytrendingstories.com/', "https://assets.mytrendingstories.com/")
-        .replace('https://cdn.mytrendingstories.com/', "https://assets.mytrendingstories.com/")
+        .replace('http://cdn.mytrendingstories.com/', "https://cdn.mytrendingstories.com/")
         .replace('https://abc2020new.com/', "https://assets.mytrendingstories.com/");
     }
     return latestURL;
--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -149,7 +149,7 @@ export class HomeComponent implements OnInit {
     let latestURL = url
     if (url) {
       latestURL = latestURL.replace('https://mytrendingstories.com/', "https://assets.mytrendingstories.com/")
-        .replace('https://cdn.mytrendingstories.com/', "https://assets.mytrendingstories.com/")
+        .replace('http://cdn.mytrendingstories.com/', "https://cdn.mytrendingstories.com/")
         .replace('https://abc2020new.com/', "https://assets.mytrendingstories.com/");
     }
     return latestURL;
