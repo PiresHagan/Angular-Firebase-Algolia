e2b16c638b255e19dbc07b05ed9e50eeb8202d24
PiresHagan <apollot7dev@gmail.com>
2020-09-18 17:56:37 +0100

    defer loading...

--- src/assets/lib/async-script-loader.js
+++ src/assets/lib/async-script-loader.js
@@ -40,7 +40,7 @@
 
           document.body.appendChild(script);
         });
-      }, 10); // @TODO: Instead of timeout, when is it best to load this?
+      }, 15000); // @TODO: Instead of timeout, when is it best to load this?
     });
   });
 })();
