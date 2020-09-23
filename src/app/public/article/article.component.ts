df4e6acb1c3ad384c96d49f39ddadebeb04f0198
PiresHagan <apollot7dev@gmail.com>
2020-09-23 15:30:16 +0530

    Added Show More and show Less on related Article

--- src/app/public/article/article.component.ts
+++ src/app/public/article/article.component.ts
@@ -464,5 +464,9 @@ export class ArticleComponent implements OnInit {
   handleCancel(): void {
     this.isVisible = false;
   }
+  SlideRelatedArticle: boolean = false;
+  slideArticle(){
+    this.SlideRelatedArticle = !this.SlideRelatedArticle;   
+  }
 }
 
