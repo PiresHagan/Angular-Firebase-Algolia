eaf9e28fb64410d1b3e804da0c1862c53df3cb6e
PiresHagan <apollot7dev@gmail.com>
2020-09-29 16:27:50 +0530

    added login check popup

--- src/app/public/shop/cart/cart-calculator/cart-calculator.component.ts
+++ src/app/public/shop/cart/cart-calculator/cart-calculator.component.ts
@@ -7,6 +7,10 @@ import {
   SimpleChanges,
 } from "@angular/core";
 import { Product } from "src/app/shared/interfaces/ecommerce/product";
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/authentication.service';
+import { User } from 'src/app/shared/interfaces/user.type';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cart-calculator',
@@ -14,11 +18,16 @@ import { Product } from "src/app/shared/interfaces/ecommerce/product";
   styleUrls: ['./cart-calculator.component.scss']
 })
 export class CartCalculatorComponent implements OnInit, OnChanges {
-
+  userDetails: User;
+  isLoggedInUser: boolean = false;
   @Input() products: Product[];
 
   totalValue = 0;
-  constructor() {}
+  constructor(
+    private router: Router,
+    public authService: AuthService,
+    public translate: TranslateService,
+  ) {}
 
   ngOnChanges(changes: SimpleChanges) {
     const dataChanges: SimpleChange = changes.products;
@@ -30,6 +39,50 @@ export class CartCalculatorComponent implements OnInit, OnChanges {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.authService.getAuthState().subscribe(async (user) => {
+      if (!user) {
+        this.userDetails = null;
+        this.isLoggedInUser = false;
+        return;
+      }
+      if (this.userDetails) {
+        this.isLoggedInUser = true;
+      } else {
+        this.isLoggedInUser = false;
+     
+      }
+  
+  
+  
+    });
+  }
+  isVisible = false;
+  isOkLoading = false;
 
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    this.isOkLoading = true;
+    setTimeout(() => {
+      this.router.navigate(["auth/login"]);
+      this.isOkLoading = false;
+    }, 2000);
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+  checkLoginCheckout(){
+    this.authService.getAuthState().subscribe(async (user) => {
+      if (!user.isAnonymous) {
+        this.router.navigate(["/shop/checkout"]);
+      } else {
+        this.showModal()
+      }
+    });
+  }
 }
