46bddf6b7e744726f9a2cf528594cb8b5dcbe6ff
PiresHagan <apollot7dev@gmail.com>
2020-10-14 21:45:27 +0530

    MTS2-411 : Removed imports for static component from public module

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,8 +2,6 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
-import { Copywriter1Component } from './copywriter1/copywriter1.component';
-import { Copywriter2Component } from './copywriter2/copywriter2.component';
 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -12,17 +10,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BuyComponent } from './buy/buy.component';
 import { createTranslateLoader, SharedModule } from '../shared/shared.module';
-import { InfluencerComponent } from './influencer/influencer.component';
 import { CloudinaryModule, CloudinaryConfiguration } from '@cloudinary/angular-5.x';
 import { Cloudinary } from 'cloudinary-core';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @NgModule({
   declarations: [
-    Copywriter1Component,
-    Copywriter2Component,
-    BuyComponent,
-    InfluencerComponent
+    BuyComponent
   ],
   imports: [
     CloudinaryModule.forRoot({ Cloudinary }, { cloud_name: 'mytrendingstories' } as CloudinaryConfiguration),
