ca8a8eb08bba70015dd11f1da84dfc189791bfaf
PiresHagan <apollot7dev@gmail.com>
2020-10-14 16:31:11 +0530

    Added content in bit coin store component in console

--- src/app/backoffice/bitcoin-store/bitcoin-store.component.ts
+++ src/app/backoffice/bitcoin-store/bitcoin-store.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bitcoin-store.component.scss']
 })
 export class BitcoinStoreComponent implements OnInit {
-
+  successCopyTxt: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  copyInputMessage(inputElement){
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
+    this.successCopyTxt= true;
+    setTimeout(() => {
+      this.successCopyTxt = false;
+    }, 2000);
+  }
 }
