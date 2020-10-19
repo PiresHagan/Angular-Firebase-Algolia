e60868d15505ed12f2f74467a3ab9878e7b1f824
PiresHagan <apollot7dev@gmail.com>
2020-10-20 01:10:23 +0530

    MTS2-416 : Added fundraiser-donate-form component

--- src/app/public/fundraiser-list/fundraiser/fundraiser.component.ts
+++ src/app/public/fundraiser-list/fundraiser/fundraiser.component.ts
@@ -1,19 +1,12 @@
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { FundraiserService } from 'src/app/shared/services/fundraiser.service';
 import { LanguageService } from 'src/app/shared/services/language.service';
 import { TranslateService } from "@ngx-translate/core";
 import { AuthService } from 'src/app/shared/services/authentication.service';
-import { NzModalService } from "ng-zorro-antd";
 import { Fundraiser } from 'src/app/shared/interfaces/fundraiser.type';
 import { User } from 'src/app/shared/interfaces/user.type';
-import { StripeService, StripeCardNumberComponent } from 'ngx-stripe';
-import {
-  StripeCardElementOptions,
-  StripeElementsOptions,
-} from '@stripe/stripe-js';
 import { AuthorService } from 'src/app/shared/services/author.service';
 // import { TEXT, AUDIO, VIDEO } from 'src/app/shared/constants/article-constants';
 import { UserService } from 'src/app/shared/services/user.service';
@@ -37,46 +30,18 @@ export class FundraiserComponent implements OnInit {
   isLoggedInUser: boolean = false;
   selectedLanguage: string = "";
   userDetails: User;
-  donateSuccess: boolean = false;
-  isFormSaving: boolean = false;
-  donateForm: FormGroup;
-  showInvalidCardError: boolean = false;
   donationPercentage: string = "0";
   authorFollowersCount: number = 0;
   // TEXT = TEXT;
   // AUDIO = AUDIO;
   // VIDEO = VIDEO;
 
-  @ViewChild(StripeCardNumberComponent) card: StripeCardNumberComponent;
-
-  cardOptions: StripeCardElementOptions = {
-    style: {
-      base: {
-        iconColor: '#666EE8',
-        color: '#31325F',
-        fontWeight: '300',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSize: '18px',
-        '::placeholder': {
-          color: '#CFD7E0'
-        }
-      }
-    }
-  };
-
-  elementsOptions: StripeElementsOptions = {
-    locale: 'en'
-  };
-
   constructor(
-    private fb: FormBuilder,
     private route: ActivatedRoute,
     private authService: AuthService,
     private langService: LanguageService,
     private fundraiserService: FundraiserService,
     private authorService: AuthorService,
-    private stripeService: StripeService,
-    private modalService: NzModalService,
     public translate: TranslateService,
     public userService: UserService,
     public companyService: CompanyService,
@@ -127,15 +92,6 @@ export class FundraiserComponent implements OnInit {
           image: { url: this.fundraiser.logo?.url },
           keywords: this.fundraiser.meta?.keyword,
         });
-      });
-
-      this.donateForm = this.fb.group({
-        first_name: [null, [Validators.required]],
-        last_name: [null, [Validators.required]],
-        email: [null, [Validators.email, Validators.required]],
-        mobile_number: [null, [Validators.required]],
-        amount: [null, [Validators.required, Validators.min(1)]],
-        message: [""]
       });
     });
   }
@@ -218,86 +174,6 @@ export class FundraiserComponent implements OnInit {
     }else{
       this.showModal()
     }
-  }
-
-  public findInvalidControls() {
-    const invalid = [];
-    const controls = this.donateForm.controls;
-
-    for (const name in controls) {
-      if (controls[name].invalid) {
-        invalid.push(name);
-      }
-    }
-
-    return invalid;
-  }
-
-  submitForm() {
-    for (const i in this.donateForm.controls) {
-      this.donateForm.controls[i].markAsDirty();
-      this.donateForm.controls[i].updateValueAndValidity();
-    }
-
-    const cardElement: any = this.card.element;
-
-    if (this.findInvalidControls().length == 0 && !cardElement._empty && !cardElement._invalid) {
-      try {
-        this.isFormSaving = true;
-
-        const name = `${this.donateForm.get('first_name').value} ${this.donateForm.get('last_name').value}`;
-
-        this.stripeService.createToken(cardElement, { name }).subscribe((result) => {
-          if (result.token) {
-            let donorData = JSON.parse(JSON.stringify(this.donateForm.value));
-            donorData['fundraiser_id'] = this.fundraiserId;
-            donorData['card_token'] = result.token.id;
-            if (donorData.message.length == 0) {
-              delete donorData.message;
-            }
-
-            this.fundraiserService.donate(donorData, this.fundraiserId).then(result => {
-              this.donateForm.reset();
-              this.card.element.clear();
-              this.isFormSaving = false;
-              this.donateSuccess = true;
-              setTimeout(() => {
-                this.donateSuccess = false;
-              }, 10000);
-            }).catch(err => {
-              this.isFormSaving = false;
-              this.showError("FundraiserAccountError");
-            });
-          } else if (result.error) {
-            this.isFormSaving = false;
-            this.showInvalidCardErr();
-          }
-        });
-      } catch (err) {
-        this.isFormSaving = false;
-      }
-    } else {
-      if (cardElement._empty || cardElement._invalid) {
-        this.showInvalidCardErr();
-      }
-
-      this.isFormSaving = false;
-    }
-  }
-
-  showInvalidCardErr() {
-    this.showInvalidCardError = true;
-
-    setTimeout(() => {
-      this.showInvalidCardError = false;
-    }, 3000);
-  }
-
-  showError(errorMessage) {
-    const msg = this.translate.instant(errorMessage);
-    this.modalService.error({
-      nzTitle: "<i>" + msg + "</i>",
-    });
   }
 
   getAuthorUrl(fundraiser) {
