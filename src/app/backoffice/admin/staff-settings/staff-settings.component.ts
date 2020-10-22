f93bc526d532cbc744ceb78e6bd52bd457609f5c
PiresHagan <apollot7dev@gmail.com>
2020-10-22 16:11:42 +0530

    MTS2-423 : Fixed the infinte scroll issue

--- src/app/backoffice/admin/staff-settings/staff-settings.component.ts
+++ src/app/backoffice/admin/staff-settings/staff-settings.component.ts
@@ -324,10 +324,11 @@ export class StaffSettingsComponent {
     this.currentUser = null;
   }
   scrollEvent = (event: any): void => {
-    if (event.target && event.target.documentElement) {
-      const top = event.target.documentElement.scrollTop
-      const height = event.target.documentElement.scrollHeight
-      const offset = event.target.documentElement.offsetHeight
+    let documentElement = event.target.documentElement ? event.target.documentElement : event.target;
+    if (documentElement) {
+      const top = documentElement.scrollTop
+      const height = documentElement.scrollHeight
+      const offset = documentElement.offsetHeight
       console.log(height, offset, top)
       if (top > height - offset - 1 - 100 && this.lastVisible && !this.loadingMore) {
         this.loadingMore = true;
