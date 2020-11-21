bb310c6d3ad0816ea080a471ffcdb9a6372d4763
PiresHagan <apollot7dev@gmail.com>
2020-11-21 11:49:03 -0500

    display token in agency

--- src/app/backoffice/agency/agency.component.ts
+++ src/app/backoffice/agency/agency.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/authentication.service';
 
 @Component({
   selector: 'app-agency',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgencyComponent implements OnInit {
 
-  constructor() { }
+  userDetails: any;
+  token: any;
+
+  constructor(
+    public authService: AuthService,
+  ) { }
 
   ngOnInit(): void {
+    /* this.authService.getAuthState().subscribe(async (user) => {
+      if (!user)
+        return;
+
+      this.userDetails = await this.authService.getLoggedInUserDetails();
+    }) */
+
+    this.authService.getIdToken().subscribe( token => {
+      this.token = token;
+      console.log('ID token', this.token);
+    });
+
+    
   }
 
 }
