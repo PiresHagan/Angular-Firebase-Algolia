5f881b5afcd3f1e94d9a603492fc548a6a79a07f
PiresHagan <apollot7dev@gmail.com>
2021-03-07 23:07:36 +0530

    MTS2-604 : Updated reset-password page

--- src/app/authentication/reset-password/reset-password.component.ts
+++ src/app/authentication/reset-password/reset-password.component.ts
@@ -4,36 +4,48 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { LanguageService } from 'src/app/shared/services/language.service';
-
+import {Location} from '@angular/common';
+import { Language } from 'src/app/shared/interfaces/language.type';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.css']
+  styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
   resetPasswordForm: FormGroup;
   errorReset: boolean = false;
   successReset: boolean = false;
+  languageList: Language[];
+  selectedLanguage: string;
 
   constructor( 
     private fb: FormBuilder, 
     public afAuth: AngularFireAuth,
     private router: Router,
     public translate: TranslateService,
-     private language: LanguageService
-    ) { }
+    private language: LanguageService,
+    private _location: Location
+  ) { }
+
+  switchLang(lang: string) {
+    this.language.changeLangOnBoarding(lang);
+  }
 
   ngOnInit() {
+    this.languageList = this.language.geLanguageList();
+
+    this.selectedLanguage = this.language.defaultLanguage;
+
     this.resetPasswordForm = this.fb.group({
-      email            : [ null, [Validators.email, Validators.required] ]
+      email: [ null, [Validators.email, Validators.required] ]
     });
   }
 
   async submitForm() {
     for (const i in this.resetPasswordForm.controls) {
-        this.resetPasswordForm.controls[ i ].markAsDirty();
-        this.resetPasswordForm.controls[ i ].updateValueAndValidity();
+      this.resetPasswordForm.controls[ i ].markAsDirty();
+      this.resetPasswordForm.controls[ i ].updateValueAndValidity();
     }
 
     this.errorReset = false;
@@ -41,23 +53,18 @@ export class ResetPasswordComponent implements OnInit {
 
     if(this.findInvalidControls().length == 0){
       try {
-            const email = this.resetPasswordForm.get('email').value;
+        const email = this.resetPasswordForm.get('email').value;
 
-            await this.afAuth.sendPasswordResetEmail(email)
-              .then(() => {
-                console.log("email sent");
-                this.successReset = true;
-
-              })
-              .catch((error) => {
-                console.log(error);
-                this.errorReset = true;
-              })
-
-        } catch (err) {
-            console.log("err...", err);
-           
-        }
+        await this.afAuth.sendPasswordResetEmail(email).then(() => {
+          console.log("email sent");
+          this.successReset = true;
+        }).catch((error) => {
+          console.log(error);
+          this.errorReset = true;
+        })
+      } catch (err) {
+        console.log("err...", err); 
+      }
     }
   }
 
@@ -65,12 +72,15 @@ export class ResetPasswordComponent implements OnInit {
     const invalid = [];
     const controls = this.resetPasswordForm.controls;
     for (const name in controls) {
-        if (controls[name].invalid) {
-            invalid.push(name);
-        }
+      if (controls[name].invalid) {
+        invalid.push(name);
+      }
     }
     return invalid;
   }
-    
+
+  backClicked() {
+    this._location.back();
+  }
 
 }
