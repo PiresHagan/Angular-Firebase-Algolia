35ecb33f78575afc4467919a95f57c4356eadb01
PiresHagan <apollot7dev@gmail.com>
2021-03-08 22:48:25 +0530

    Deleted onboarding module after merging to authentication module

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -4,9 +4,5 @@ export const FullLayout_ROUTES: Routes = [
     {
         path: '',
         loadChildren: () => import('../../authentication/authentication.module').then(m => m.AuthenticationModule)
-    },
-    {
-        path: 'onboarding',
-        loadChildren: () => import('../../onboarding/onboarding.module').then(m => m.OnboardingModule)
     }
 ];
