45839fa64f67c643f42e222abcb9074744d85a6e
PiresHagan <apollot7dev@gmail.com>
2021-04-09 20:34:37 +0530

    MTS2-724 : Added support to show company, charity and fundraisers

--- src/app/public/profile/profile.component.ts
+++ src/app/public/profile/profile.component.ts
@@ -8,7 +8,7 @@ import { UserService } from 'src/app/shared/services/user.service';
 import { environment } from 'src/environments/environment';
 import { LanguageService } from 'src/app/shared/services/language.service';
 import { Article } from 'src/app/shared/interfaces/article.type';
-import { AUTHOR } from 'src/app/shared/constants/member-constant';
+import { AUTHOR, MEMBER, CHARITY, COMPANY, FUNDRAISER } from 'src/app/shared/constants/member-constant';
 import { NzModalService } from 'ng-zorro-antd';
 import { SeoService } from 'src/app/shared/services/seo/seo.service';
 import { AnalyticsService } from 'src/app/shared/services/analytics/analytics.service';
@@ -19,7 +19,11 @@ import { AnalyticsService } from 'src/app/shared/services/analytics/analytics.se
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
+  AUTHOR = AUTHOR;
+  MEMBER = MEMBER; 
+  CHARITY = CHARITY; 
+  COMPANY = COMPANY; 
+  FUNDRAISER = FUNDRAISER; 
   authorDetails: any = {};
   followers: any = [];
   subscribers: any = [];
