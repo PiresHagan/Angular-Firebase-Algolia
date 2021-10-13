f7b26fcc2f95f61055f1d8773da4a75e12e0c73b
PiresHagan <apollot7dev@gmail.com>
2021-10-13 13:02:24 +0530

    Fixed undefined seen in SEO meta data of member url

--- src/app/shared/services/seo/seo.service.ts
+++ src/app/shared/services/seo/seo.service.ts
@@ -40,16 +40,16 @@ export class SeoService {
     }
 
     this.meta.updateTag({ name: 'twitter:title', content: config.title });
-    this.meta.updateTag({ name: 'twitter:description', content: config.description });
-    this.meta.updateTag({ name: 'twitter:card', content: config.summary || config.description });
+    this.meta.updateTag({ name: 'twitter:description', content: config.description || '' });
+    this.meta.updateTag({ name: 'twitter:card', content: config.summary || config.description || '' });
     this.meta.updateTag({ name: 'twitter:image', content: config.image.url });
 
-    this.meta.updateTag({ name: 'description', content: config.description });
+    this.meta.updateTag({ name: 'description', content: config.description || '' });
     this.meta.updateTag({ name: 'image', content: config.image.url });
     this.meta.updateTag({ name: 'keywords', content: config.keywords || this.fallbackKeyWords(config.title) });
 
     this.meta.updateTag({ property: 'og:title', content: config.title });
-    this.meta.updateTag({ property: 'og:description', content: config.description });
+    this.meta.updateTag({ property: 'og:description', content: config.description || '' });
     this.meta.updateTag({ property: 'og:image', content: config.image.url });
     // this.meta.updateTag({ property: 'og:type', content: config.type });
     this.meta.updateTag({ property: 'og:url', content: config.url });
