f34215443f125687b3f2f7f92a037f150f8bca82
PiresHagan <apollot7dev@gmail.com>
2022-03-14 04:11:34 +0530

    Resolved merge conflicts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,31 +1,33 @@
-import { NgModule } from "@angular/core";
+import { CloudinaryModule, CloudinaryConfiguration } from '@cloudinary/angular-5.x';
+import { Cloudinary } from 'cloudinary-core';
 import { CommonModule } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { RouterModule } from "@angular/router";
-import { NgZorroAntdModule } from "ng-zorro-antd";
-import { PerfectScrollbarModule } from "ngx-perfect-scrollbar";
-import { ThemeConstantService } from "./services/theme-constant.service";
-import { SearchPipe } from "./pipes/search.pipe";
-import { StripTagsPipe } from "./pipes/striptags.pipe";
-import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
-import { TranslateHttpLoader } from "@ngx-translate/http-loader";
-import { ImgSizePipe } from "./pipes/img-size.pipe";
-import { ArticleInteractionComponent } from "./component/article-interaction/article-interaction.component";
-import { ArticleAvatarComponent } from "./component/article-avatar/article-avatar.component";
-import { CloudinaryImgComponent } from "./component/cloudinary-img/cloudinary-img.component";
-import {
-  CloudinaryModule,
-  CloudinaryConfiguration,
-} from "@cloudinary/angular-5.x";
-import { Cloudinary } from "cloudinary-core";
-import { ShareButtonsComponent } from "./component/share-buttons/share-buttons.component";
-import { AdDirective } from "./directives/ad/ad.directive";
-import { SeoService } from "./services/seo/seo.service";
-import { QuicklinkModule } from "ngx-quicklink";
-import { CharityDonateFormComponent } from "./component/charity-donate-form/charity-donate-form.component";
-import { CompanyLeadFormComponent } from "./component/company-lead-form/company-lead-form.component";
-import { FundraiserDonateFormComponent } from "./component/fundraiser-donate-form/fundraiser-donate-form.component";
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { ArticleInteractionComponent } from './component/article-interaction/article-interaction.component';
+import { ArticleAvatarComponent } from './component/article-avatar/article-avatar.component';
+import { CloudinaryImgComponent } from './component/cloudinary-img/cloudinary-img.component';
+import { ShareButtonsComponent } from './component/share-buttons/share-buttons.component';
+import { ImgSizePipe } from './pipes/img-size.pipe';
+import { SearchPipe } from './pipes/search.pipe';
+import { ShopProductAddReviewComponent } from './component/shop-product-add-review/shop-product-add-review.component';
+import { ShopProductCardComponent } from './component/shop-product-card/shop-product-card.component';
+import { StoreComponent } from './component/store/store.component';
+import { StripTagsPipe } from './pipes/striptags.pipe';
+import { ThemeConstantService } from './services/theme-constant.service';
+import { ProductStarRatingComponent } from './component/product-star-rating/product-star-rating.component';
+import { AdDirective } from './directives/ad/ad.directive';
+import { SeoService } from './services/seo/seo.service';
+import { QuicklinkModule } from 'ngx-quicklink';
+import { CharityDonateFormComponent } from './component/charity-donate-form/charity-donate-form.component';
+import { CompanyLeadFormComponent } from './component/company-lead-form/company-lead-form.component';
+import { FundraiserDonateFormComponent } from './component/fundraiser-donate-form/fundraiser-donate-form.component';
 
 import { NgxStripeModule } from "ngx-stripe";
 import { environment } from "src/environments/environment";
@@ -37,62 +39,74 @@ export function createTranslateLoader(http: HttpClient) {
 }
 
 @NgModule({
-  exports: [
-    CommonModule,
-    FormsModule,
-    HttpClientModule,
-    NgZorroAntdModule,
-    PerfectScrollbarModule,
-    SearchPipe,
-    ArticleInteractionComponent,
-    ArticleAvatarComponent,
-    CloudinaryImgComponent,
-    ShareButtonsComponent,
-    AdDirective,
-    QuicklinkModule,
-    CharityDonateFormComponent,
-    CompanyLeadFormComponent,
-    FundraiserDonateFormComponent,
-    CloudinaryFeatureImgComponent,
-    FluidToolbarComponent,
-  ],
-  imports: [
-    RouterModule,
-    CommonModule,
-    NgZorroAntdModule,
-    FormsModule,
-    ReactiveFormsModule,
-    PerfectScrollbarModule,
-    QuicklinkModule,
-    CloudinaryModule.forRoot({ Cloudinary }, {
-      cloud_name: "mytrendingstories",
-    } as CloudinaryConfiguration),
-    NgxStripeModule.forRoot(environment.stripePublishableKey),
-    TranslateModule.forChild({
-      useDefaultLang: true,
-      isolate: false,
-      loader: {
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [HttpClient],
-      },
-    }),
-  ],
-  declarations: [
-    SearchPipe,
-    StripTagsPipe,
-    ImgSizePipe,
-    ArticleInteractionComponent,
-    ArticleAvatarComponent,
-    CloudinaryImgComponent,
-    ShareButtonsComponent,
-    AdDirective,
-    CharityDonateFormComponent,
-    CompanyLeadFormComponent,
-    FundraiserDonateFormComponent,
-    CloudinaryFeatureImgComponent,
-    FluidToolbarComponent,
-  ],
-  providers: [ThemeConstantService, SeoService],
+    exports: [
+      CommonModule,
+      FormsModule,
+      ReactiveFormsModule,
+      HttpClientModule,
+      NgZorroAntdModule,
+      PerfectScrollbarModule,
+      SearchPipe,
+      ArticleInteractionComponent,
+      ArticleAvatarComponent,
+      CloudinaryImgComponent,
+      ShareButtonsComponent,
+      ShopProductCardComponent,
+      StoreComponent,
+      ShopProductAddReviewComponent,
+      ProductStarRatingComponent,
+      AdDirective,
+      QuicklinkModule,
+      CharityDonateFormComponent,
+      CompanyLeadFormComponent,
+      FundraiserDonateFormComponent,
+      CloudinaryFeatureImgComponent,
+      CommonModule,
+      FluidToolbarComponent,
+    ],
+    imports: [
+      RouterModule,
+      CommonModule,
+      FormsModule,
+      NgZorroAntdModule,
+      FormsModule,
+      ReactiveFormsModule,
+      PerfectScrollbarModule,
+      QuicklinkModule,
+      CloudinaryModule.forRoot({ Cloudinary }, { cloud_name: 'mytrendingstories' } as CloudinaryConfiguration),
+      NgxStripeModule.forRoot(environment.stripePublishableKey),
+      TranslateModule.forChild({
+        useDefaultLang: true,
+        isolate: false,
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        }
+      })
+    ],
+    declarations: [
+        SearchPipe,
+        StripTagsPipe,
+        ImgSizePipe,
+        ArticleInteractionComponent,
+        ArticleAvatarComponent,
+        CloudinaryImgComponent,
+        ShareButtonsComponent,
+        AdDirective,
+        CharityDonateFormComponent,
+        CompanyLeadFormComponent,
+        FundraiserDonateFormComponent,
+        ShopProductAddReviewComponent,
+        ShopProductCardComponent,
+        ProductStarRatingComponent,
+        StoreComponent,
+        CloudinaryFeatureImgComponent,
+        FluidToolbarComponent
+    ],
+    providers: [
+        ThemeConstantService,
+        SeoService,
+    ],
 })
 export class SharedModule {}
