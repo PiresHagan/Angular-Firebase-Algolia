465aef365af9c0918a9bb958d003d297f5f2b7a2
PiresHagan <apollot7dev@gmail.com>
2022-07-19 16:14:40 +0000

    implement logo for each user type

--- src/app/shared/component/article-avatar/article-avatar.component.ts
+++ src/app/shared/component/article-avatar/article-avatar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/article.type';
+import { AuthorService } from '../../services/author.service';
 @Component({
   selector: 'app-article-avatar',
   templateUrl: './article-avatar.component.html',
@@ -7,10 +8,32 @@ import { Article } from '../../interfaces/article.type';
 })
 export class ArticleAvatarComponent implements OnInit {
   @Input() article: Article;
-  constructor() {
+  authorDetail: any;
+  userType: any;
+  authorId: string;
+  public isGuestPostEnabled = false;
 
-  }
+  constructor(
+    private authorService: AuthorService) {}
+
   ngOnInit(): void {
+    this.authorId = this.article?.author?.id;
+    this.getUsertype(this.authorId)
+    this.authorService.getAuthorTypeById(this.authorId).subscribe(data => {
+      this.authorDetail = data;
+      if(this.authorDetail[0]?.is_guest_post_enabled) {
+        this.isGuestPostEnabled = this.authorDetail[0]?.is_guest_post_enabled;
+      }
+    })
+  }
+
+  getUsertype(authorId){
+    this.authorService.getAuthorTypeById(authorId).subscribe(data => {
+      this.authorDetail = data;
+      if(this.authorDetail[0]?.user_type) {
+        return this.userType = this.authorDetail[0]?.user_type;
+      }
+    })
   }
  
 }
