747ae83db6793c1bcf088570befcb8942f5a1a5c
PiresHagan <apollot7dev@gmail.com>
2022-08-24 17:26:46 +0000

    Update onboarding pending page messages for all 20 user type

--- src/app/authentication/_const/preferences.ts
+++ src/app/authentication/_const/preferences.ts
@@ -28,4 +28,52 @@ export const preferences: Preference[] = [
     type: "ecommerce",
     description: "description.ecommerce",
   },
+  {
+    type: "advisor",
+    description: "description.advisor",
+  },
+  {
+    type: "online-courses",
+    description: "description.online-courses",
+  },
+  {
+    type: "services",
+    description: "description.services",
+  },
+  {
+    type: "job",
+    description: "description.job",
+  },
+  {
+    type: "host-event",
+    description: "description.host-event",
+  },
+  {
+    type: "influencer-marketing",
+    description: "description.influencer-marketing",
+  },
+  {
+    type: "guest-post",
+    description: "description.guest-post",
+  }, 
+  {
+    type: "paid-premium-group",
+    description: "description.paid-premium-group",
+  },
+  {
+    type: "restaurant",
+    description: "description.restaurant",
+  }, 
+  {
+    type: "politician",
+    description: "description.politician",
+  },
+  {
+    type: "investment",
+    description: "description.investment",
+  },
+  {
+    type: "vacations-rentals",
+    description: "description.vacations-rentals",
+  },
 ];
